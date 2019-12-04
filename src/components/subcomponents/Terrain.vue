124fd5df598ac3bc231314b6478c2a0304410a1f
fengfeng043 <729964264@qq.com>
2019-12-04 14:55:47 +0800

    地形

--- src/components/subcomponents/Terrain.vue
+++ src/components/subcomponents/Terrain.vue
@@ -17,20 +17,38 @@
                     <Col span="12">
 
 
-                    <!--<FormItem label="选择模型位置" label-position="top">-->
-                        <!--<br>-->
-                        <!--<Input class="input" v-model="location"-->
-                               <!--placeholder="120 31 15000" style="width: 300px"/>-->
-
-
-                    <!--</FormItem>-->
+                    <div id="zoomButtons"></div>
+                    <div class="demo-container">
+                        <label><input type="radio" name="shadingMaterials" value="none" data-bind="checked: selectedShading"> No shading</label>
+                        <label><input type="radio" name="shadingMaterials" value="elevation" data-bind="checked: selectedShading"> Elevation</label>
+                        <label><input type="radio" name="shadingMaterials" value="slope" data-bind="checked: selectedShading"> Slope</label>
+                        <label><input type="radio" name="shadingMaterials" value="aspect" data-bind="checked: selectedShading"> Aspect</label>
+                    </div>
+                    <div class="demo-container">
+                        <div>
+                            <label><input type="checkbox" data-bind="checked: enableContour">Enable Contour Lines</label>
+                        </div>
+                        <div>
+                            Spacing <input style="width: 136px" type="range" min="1.0" max="500.0" step="1.0" data-bind="value: contourSpacing, valueUpdate: 'input', enable: enableContour"> <span data-bind="text: contourSpacing"></span>m
+                        </div>
+                        <div>
+                            Line Width <input style="width: 125px" type="range" min="1.0" max="10.0" step="1.0" data-bind="value: contourWidth, valueUpdate: 'input', enable: enableContour"> <span data-bind="text: contourWidth"></span>px
+                        </div>
+                        <div>
+                            <button type="button" data-bind="click: changeColor, enable: enableContour">Change contour color</button>
+                        </div>
+                    </div>
                     </Col>
 
                 </Row>
             </Form>
             <div class="demo-drawer-footer">
-                <Button style="margin-right: 8px" @click="cancel">取消</Button>
-                <Button type="primary" @click="submit">生效</Button>
+                <Button style="margin-right: 8px" @click="cancel">取消地形</Button>
+                <Button type="text" @click="submit">地形生效</Button>
+
+                <Button type="text" @click="elevation">坡度地形渲染</Button>
+
+
             </div>
         </Drawer>
 
@@ -89,7 +107,31 @@
 
 
             cancel() {
-                this.value3 = false;
+//                this.value3 = false;
+                this.$Message.info('加载');
+                var viewer = this.myMap.viewer;
+                var Cesium = this.myMap.Cesium;
+                var cesiumTerrainProvider = Cesium.createWorldTerrain();
+                var ellipsoidTerrainProvider = new Cesium.EllipsoidTerrainProvider();
+
+                // 地形生效
+//                viewer.scene.terrainProvider = cesiumTerrainProvider;
+                // 地形平坦
+                viewer.scene.terrainProvider = ellipsoidTerrainProvider;
+            },
+            elevation(){
+                this.$Message.info('加载');
+                var viewer = this.myMap.viewer;
+                var Cesium = this.myMap.Cesium;
+
+                var viewer = new Cesium.Viewer('cesiumContainer', {
+                    terrainProvider: Cesium.createWorldTerrain({
+                        requestVertexNormals: true //Needed to visualize slope
+                    })
+                });
+                viewer.scene.globe.enableLighting = true;
+
+
             }
         },
         components: {
